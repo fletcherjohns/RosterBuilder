daea443d93f47576ed88455fb51f4fffba56137d
Fletcher Johns <fletcherjohns82@gmail.com>
2015-09-16 22:32:20 +1000

    added Fling etc

--- staffrosterviews/src/main/java/au/com/psilisoft/www/staffrosterviews/CustomNumberPicker.java
+++ staffrosterviews/src/main/java/au/com/psilisoft/www/staffrosterviews/CustomNumberPicker.java
@@ -10,29 +10,30 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.Random;
 
 /**
  * Created by Fletcher on 14/09/2015.
  */
 public class CustomNumberPicker extends View {
 
+    private static final float FLING_VELOCITY_THRESHOLD = 0.01f;
     private int mMin;
     private int mMax;
     private int mIncrement;
+    private int mNumberOfSides = 16;
+
     private int mCount;
     private double mScrollPosition = 0;
 
     private int mWidth;
     private int mHeight;
+
     private int mBitmapWidth;
     private int mBitmapHeight;
-    private float mRadius;
+
     private float mApothem;
 
     private Paint mTextPaint;
@@ -41,6 +42,9 @@ public class CustomNumberPicker extends View {
     private Matrix mMatrix;
 
     private GestureDetector mGestureDetector;
+    private float mScrollVelocity;
+    private SnapThread mSnapThread;
+    private FlingThread mFlingThread;
 
 
     public CustomNumberPicker(Context context, AttributeSet attrs) {
@@ -66,24 +70,28 @@ public class CustomNumberPicker extends View {
         mTextPaint = new Paint();
         mTextPaint.setColor(Color.BLACK);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
+
         mBitmapPaint = new Paint();
         mBitmapPaint.setFilterBitmap(true);
         mBitmapPaint.setAntiAlias(true);
+
         mCamera = new Camera();
         mMatrix = new Matrix();
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
 
-        mWidth = r - l;
-        mHeight = b - t;
-        mBitmapHeight = (int) (mHeight / Math.sqrt(4 + 2 * Math.sqrt(2)) * 1.8);
+        mWidth = w;
+        mHeight = h;
+
+        float radius = (float) (mHeight);
+        mApothem = (float) (radius * Math.cos(Math.PI / mNumberOfSides));
+
+        mBitmapHeight = (int) (radius * 2 * Math.sin(Math.PI / mNumberOfSides));
         mBitmapWidth = (int) (mWidth * .8);
-        mRadius = (float) (mBitmapHeight / (2. * Math.sin(Math.PI / 8.)));
-        mApothem = (float) (mRadius * Math.cos(Math.PI / 8.));
 
-        Log.v("password", "radius = " + mRadius + ", apothem = " + mApothem);
         mTextPaint.setTextSize(mBitmapHeight / 2);
     }
 
@@ -93,11 +101,16 @@ public class CustomNumberPicker extends View {
 
         int centrePosition = (int) Math.round(mScrollPosition);
 
-        for (int i = -2; i <=0; i++) {
-            drawBitmap(canvas, centrePosition + i);
-            if (i != 0) {
-                drawBitmap(canvas, centrePosition - i);
+        if (mScrollVelocity != 0) {
+            for (int i = -(mNumberOfSides / 4); i <= 0; i++) {
+
+                drawBitmap(canvas, centrePosition + i);
+                if (i != 0) {
+                    drawBitmap(canvas, centrePosition - i);
+                }
             }
+        } else {
+            drawBitmap(canvas, centrePosition);
         }
     }
 
@@ -107,36 +120,35 @@ public class CustomNumberPicker extends View {
         if (value < 0) {
             value += mCount;
         }
-        value *= mIncrement;
+        value = mMin + value * mIncrement;
 
         Bitmap b = getBitmap(value);
-        setMatrix(position);
-        RectF rect = new RectF(0, 0, mBitmapWidth, mBitmapHeight);
-        mMatrix.mapRect(rect);
-        Log.v("password", "left: " + rect.left + ", top: " + rect.top + ", right: " + rect.right + ", bottom: " + rect.bottom);
-        if (rect.height() > 0) {
+        if (setMatrix(position)) {
             canvas.drawBitmap(b, mMatrix, mBitmapPaint);
         }
-    }
 
+    }
 
 
     private Bitmap getBitmap(int value) {
 
         Bitmap b = Bitmap.createBitmap(mBitmapWidth, mBitmapHeight, Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(b);
-        c.drawColor(Color.WHITE);
         float textHeight = mTextPaint.getTextSize();
         c.drawText(String.valueOf(value), mBitmapWidth / 2, mBitmapHeight / 2 + textHeight / 2, mTextPaint);
         return b;
     }
 
-    private void setMatrix(int position) {
+    private boolean setMatrix(int position) {
 
         RectF rect = getChildDimensions();
 
-        float rotation = (float) (mScrollPosition - position) * 45;
+        float rotation = (float) ((mScrollPosition - position) * 360 / mNumberOfSides);
+        if (Math.abs(rotation) > 40) {
+            return false;
+        }
         mCamera.save();
+
         mCamera.translate(0, 0, mApothem);
         mCamera.rotateX(rotation);
         mCamera.translate(0, 0, -mApothem);
@@ -145,6 +157,8 @@ public class CustomNumberPicker extends View {
 
         mMatrix.preTranslate(-mBitmapWidth / 2, -mBitmapHeight / 2);
         mMatrix.postTranslate(mBitmapWidth / 2 + rect.left, mBitmapHeight / 2 + rect.top);
+
+        return true;
     }
 
     private RectF getChildDimensions() {
@@ -163,22 +177,104 @@ public class CustomNumberPicker extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        mGestureDetector.onTouchEvent(event);
-        return true;
+        if (!mGestureDetector.onTouchEvent(event)) {
+            mSnapThread = new SnapThread((int) Math.round(mScrollPosition));
+            mSnapThread.start();
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private class GestureDetectorListener extends android.view.GestureDetector.SimpleOnGestureListener {
         @Override
+        public boolean onDown(MotionEvent e) {
+            try {
+                mFlingThread.interrupt();
+            } catch (Exception ex) {
+                // Thread probably null
+            }
+            try {
+                mSnapThread.interrupt();
+            } catch (Exception ex) {
+                // Thread probably null
+            }
+            return true;
+        }
+
+        @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
             mScrollPosition += distanceY / mBitmapHeight;
             mScrollPosition %= mCount;
+            mScrollVelocity = 1;
             invalidate();
             return true;
         }
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return super.onFling(e1, e2, velocityX, velocityY);
+            mScrollVelocity = -velocityY / 80000;
+            mFlingThread = new FlingThread();
+            mFlingThread.start();
+            return true;
+        }
+
+    }
+
+    private class FlingThread extends Thread {
+
+        @Override
+        public void run() {
+
+            while (Math.abs(mScrollVelocity) > FLING_VELOCITY_THRESHOLD) {
+                mScrollVelocity *= 0.95f;
+
+                mScrollPosition += mScrollVelocity;
+                mScrollPosition %= mCount;
+                post(new RequestLayoutRunnable());
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+            mSnapThread = new SnapThread((int) Math.round(mScrollPosition));
+            mSnapThread.start();
+        }
+    }
+
+    private class SnapThread extends Thread {
+
+        int mSnapPosition;
+
+        public SnapThread(int snapPosition) {
+            mSnapPosition = snapPosition;
+        }
+
+        @Override
+        public void run() {
+
+            while (Math.abs((mSnapPosition - mScrollPosition)) > 0.001) {
+                mScrollVelocity = (float) ((mSnapPosition - mScrollPosition) / 10);
+                mScrollPosition += mScrollVelocity;
+                post(new RequestLayoutRunnable());
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+            mScrollVelocity = 0;
+            mScrollPosition = mSnapPosition;
+            post(new RequestLayoutRunnable());
+        }
+    }
+
+    private class RequestLayoutRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            invalidate();
         }
     }
 }
